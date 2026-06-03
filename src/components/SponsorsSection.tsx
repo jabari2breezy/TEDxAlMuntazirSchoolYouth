4dcbc04343b98c7f72294741d4df788b41ac7e3b
jabari2breezy <jabari2breezy@users.noreply.github.com>
2026-06-03 11:48:27 +0300

    Add Hisense to platinum sponsors

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -29,6 +29,7 @@ const SPONSOR_TIERS: SponsorTier[] = [
     sponsors: [
       { src: '/sponsors/etg-hd.png', alt: 'ETG', isETG: true },
       { src: '/sponsors/asas-updated.png', alt: 'ASAS' },
+      { src: 'https://assets.stickpng.com/images/600da560516067000439ac8e.png', alt: 'Hisense' },
     ],
   },
   {
